4c0fea5594624169b5cb69ff806cba4f9fc6b560
Elariondakta <prevottheodore@gmail.com>
2020-03-03 17:16:31 +0100

    code fini

--- karting_compte_tour.ino
+++ karting_compte_tour.ino
@@ -1,14 +1,67 @@
-#define receiverPin 8
-#define maxKartingTour 11000
+
+#define receiverPin 7
+#define maxKartingTour 233 //Nombre de tour max par seconde du moteur
 
 bool values[1000] = {false}; //représente une seconde, pas de 1 milliseconde
 int oldTimestamp = 0;
 int offset = 0;
 
+
+int SER_Pin = 8;   //pin 14 on the 75HC595  //
+int RCLK_Pin = 9;  //pin 12 on the 75HC595
+int SRCLK_Pin = 10; //pin 11 on the 75HC595
+ 
+#define number_of_74hc595s 1 //Pas touche jsp ce que c'est ^^
+ 
+//do not touch (Ca non plus d'ailleur il le dit aussi le monsieur)
+#define numOfRegisterPins number_of_74hc595s * 8
+ 
+boolean registers[numOfRegisterPins];   
+ 
+//set all register pins to LOW
+void clearRegisters(){
+  for(int i = numOfRegisterPins - 1; i >=  0; i--){
+     registers[i] = LOW;
+  }
+} 
+ 
+//Set and display registers
+//Only call AFTER all values are set how you would like (slow otherwise)
+void writeRegisters(){
+ 
+  digitalWrite(RCLK_Pin, LOW);
+ 
+  for(int i = numOfRegisterPins - 1; i >=  0; i--){
+    digitalWrite(SRCLK_Pin, LOW);
+ 
+    int val = registers[i];
+ 
+    digitalWrite(SER_Pin, val);
+    digitalWrite(SRCLK_Pin, HIGH);
+ 
+  }
+  digitalWrite(RCLK_Pin, HIGH);
+ 
+}
+ 
+//set an individual pin HIGH or LOW
+void setRegisterPin(int index, int value){
+  registers[index] = value;
+}
+
 void setup() {
   Serial.begin(9600);
-  pinMode(receiverPin, INPUT);
+  
   oldTimestamp = 0;
+
+  pinMode(receiverPin, INPUT);
+  pinMode(SER_Pin, OUTPUT);
+  pinMode(RCLK_Pin, OUTPUT);
+  pinMode(SRCLK_Pin, OUTPUT);
+ 
+  //reset all register pins
+  clearRegisters();
+  writeRegisters();
 }
 
 void loop() {
@@ -35,12 +88,20 @@ void loop() {
     }
     Serial.print("Nombre de tour : ");
     Serial.println(count);
-    float ratio = count/maxKartingTour;
-    led_index = (int)(ratio*10)
     
+    float ratio = (float)count/(float)maxKartingTour;
     
-    //delay(5000);
+    if (ratio > 1) ratio = 1;
+    int led_index = (int)(ratio*10);
+    
+    clearRegisters();
+    
+    for (int i = 1; i < led_index - 2; i++) { //-2 parce que on a 2 leds en moins
+      setRegisterPin(i, HIGH);
+    }
+    writeRegisters();
+       
     offset = 0;
   }
-  delay(1);
+  delay(1); // !! Ne pas enlever !! Permet au programme de pas planter !! //
 }
